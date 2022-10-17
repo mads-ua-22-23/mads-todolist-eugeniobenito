7cdd524371338608b09ec2915dcc7683614f838b
Eugenio Benito <eugeniobenito00@gmail.com>
2022-10-17 10:28:56 +0200

    Añade endpoint en UsuarioController y vista

--- src/test/java/madstodolist/UsuarioWebTest.java
+++ src/test/java/madstodolist/UsuarioWebTest.java
@@ -23,42 +23,42 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 // moquear el usuarioService.
 public class UsuarioWebTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+        @Autowired
+        private MockMvc mockMvc;
 
-    // Moqueamos el usuarioService.
-    // En los tests deberemos proporcionar el valor devuelto por las llamadas
-    // a los métodos de usuarioService que se van a ejecutar cuando se realicen
-    // las peticiones a los endpoint.
-    @MockBean
-    private UsuarioService usuarioService;
+        // Moqueamos el usuarioService.
+        // En los tests deberemos proporcionar el valor devuelto por las llamadas
+        // a los métodos de usuarioService que se van a ejecutar cuando se realicen
+        // las peticiones a los endpoint.
+        @MockBean
+        private UsuarioService usuarioService;
 
-    @Test
-    public void servicioLoginUsuarioOK() throws Exception {
-        // GIVEN
-        // Moqueamos la llamada a usuarioService.login para que
-        // devuelva un LOGIN_OK y la llamada a usuarioServicie.findByEmail
-        // para que devuelva un usuario determinado.
+        @Test
+        public void servicioLoginUsuarioOK() throws Exception {
+                // GIVEN
+                // Moqueamos la llamada a usuarioService.login para que
+                // devuelva un LOGIN_OK y la llamada a usuarioServicie.findByEmail
+                // para que devuelva un usuario determinado.
 
-        Usuario anaGarcia = new Usuario("ana.garcia@gmail.com");
-        anaGarcia.setId(1L);
+                Usuario anaGarcia = new Usuario("ana.garcia@gmail.com");
+                anaGarcia.setId(1L);
 
-        when(usuarioService.login("ana.garcia@gmail.com", "12345678"))
-                .thenReturn(UsuarioService.LoginStatus.LOGIN_OK);
-        when(usuarioService.findByEmail("ana.garcia@gmail.com"))
-                .thenReturn(anaGarcia);
+                when(usuarioService.login("ana.garcia@gmail.com", "12345678"))
+                                .thenReturn(UsuarioService.LoginStatus.LOGIN_OK);
+                when(usuarioService.findByEmail("ana.garcia@gmail.com"))
+                                .thenReturn(anaGarcia);
 
-        // WHEN, THEN
-        // Realizamos una petición POST al login pasando los datos
-        // esperados en el mock, la petición devolverá una redirección a la
-        // URL con las tareas del usuario
+                // WHEN, THEN
+                // Realizamos una petición POST al login pasando los datos
+                // esperados en el mock, la petición devolverá una redirección a la
+                // URL con las tareas del usuario
 
-        this.mockMvc.perform(post("/login")
-                        .param("eMail", "ana.garcia@gmail.com")
-                        .param("password", "12345678"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/usuarios/1/tareas"));
-    }
+                this.mockMvc.perform(post("/login")
+                                .param("eMail", "ana.garcia@gmail.com")
+                                .param("password", "12345678"))
+                                .andExpect(status().is3xxRedirection())
+                                .andExpect(redirectedUrl("/usuarios/1/tareas"));
+        }
 
     @Test
     public void servicioLoginUsuarioNotFound() throws Exception {
@@ -93,18 +93,45 @@ public class UsuarioWebTest {
                         .param("password","000"))
                 .andExpect(content().string(containsString("Contraseña incorrecta")));
     }
-    
-    @Test
-    public void servicioListarUsuarios() throws Exception {
 
-        // WHEN, THEN
-        // Realizamos una petición get con la lista de usuarios
-        this.mockMvc.perform(get("/registrados"))
-        .andExpect((content().string(allOf(
-                containsString("Lista de usuarios"),
-                containsString("Id"),
-                containsString("Email"),
-                containsString("Total usuarios:")
-        ))));
-    }
+        @Test
+        public void servicioListarUsuarios() throws Exception {
+
+                // WHEN, THEN
+                // Realizamos una petición get con la lista de usuarios
+                this.mockMvc.perform(get("/registrados"))
+                                .andExpect((content().string(allOf(
+                                                containsString("Lista de usuarios"),
+                                                containsString("Id"),
+                                                containsString("Email"),
+                                                containsString("Total usuarios:")))));
+        }
+
+        @Test
+        public void servicioDescripciónUsuario() throws Exception {
+                
+                // GIVEN
+                // Un usuario con correo y Id
+                Usuario usuario = new Usuario("user@ua");
+                usuario.setNombre("Usuario Ejemplo");
+                usuario.setId(1L);
+
+                // WHEN
+                // Mockeamos el servicio de búsqueda por Id para que nos devuelva el 
+                // usuario que acabamos de crear
+                when(usuarioService.findById(usuario.getId())).thenReturn(usuario);
+
+                // THEN
+                // Se realiza la petición GET a la descipción del usuario,
+                // el HTML devuelto contiene la información de nuestro usuario
+                this.mockMvc.perform(get("/registrados/1"))
+                .andExpect((content().string(allOf(
+                                containsString("Descripción de user@ua"),
+                                containsString("Id"),
+                                containsString("Nombre"),
+                                containsString("Email"),
+                                containsString("Fecha de nacimiento"),
+                                containsString("Usuario Ejemplo"),
+                                containsString("user@ua")))));
+        }
 }
