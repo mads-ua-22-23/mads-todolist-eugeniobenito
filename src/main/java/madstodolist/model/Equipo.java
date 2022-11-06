4c4d0974c13b1a7354665dc3fb1077cc49b3cbb0
Eugenio Benito <eugeniobenito00@gmail.com>
2022-11-06 15:35:33 +0100

    Primer commit - Clase Equipo

---
+++ src/main/java/madstodolist/model/Equipo.java
@@ -0,0 +1,13 @@
+package madstodolist.model;
+
+public class Equipo {
+    private String nombre;
+
+    public Equipo(String nombre) {
+        this.nombre = nombre;
+    }    
+
+    public String getNombre() {
+        return this.nombre;
+    }
+}
